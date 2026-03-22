13005b9bbbf877d890f2fa40029e0f5f5190a521
Jackelineu <jmaldonadol2@miumg.edu.gt>
2026-03-22 15:43:50 -0600

    Update ProgralGrupo32026.java

---
+++ Grupo3PrograIB2026/src/main/java/gt/edu/miumg/grupo3prograib2026/Grupo3PrograIB2026.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gt.edu.miumg.grupo3prograib2026;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Grupo3PrograIB2026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
